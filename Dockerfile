78fb8830825fe1e7761e2c264e8f96b2901c6b86
Jon Davis <jon.davis@smule.com>
2018-08-20 13:14:12 -0700

    refining the build slightly

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,6 @@ FROM phusion/baseimage:latest
 MAINTAINER Jon Davis
 
 # Set the locale
-#RUN locale-gen en_US.UTF-8
-#ENV LANG en_US.UTF-8
-#ENV LANGUAGE en_US:en
-#ENV LC_ALL en_US.UTF-8
 RUN echo en_US.UTF-8 UTF-8 > /var/lib/locales/supported.d/en
 
 COPY installpkg /usr/sbin/installpkg
@@ -13,7 +9,6 @@ RUN chmod 755 /usr/sbin/installpkg
 
 RUN apt-get -qq update \
  && apt-get dist-upgrade -y --no-install-recommends -o Dpkg::Options::="--force-confold" \
- && apt-get -y install nano \
  && apt-get clean \
  && apt-get autoremove -y --purge \
  && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-today=`date +%Y%m%d`
+USERNAME="shakataganai"
+TODAY=`date +%Y%m%d`
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-echo "Building minibuntu:$today"
+NAME="$(cut -d- -f2- <<< ${PWD##*/})"
 
-docker build . --no-cache=true -t shakataganai/miniubuntu:$today
-docker tag shakataganai/miniubuntu:$today  shakataganai/miniubuntu:latest
-docker push shakataganai/miniubuntu:$today
-docker push shakataganai/miniubuntu:latest
-
+if [ -z "$TRAVIS" ]; then    
+    echo "LOCAL Build -- $NAME:latest"
+    docker build . -t $USERNAME/$NAME:latest
+else
+    echo "CI/CD Build -- $NAME:$TODAY"
+    docker build . --no-cache=true -t $USERNAME/$NAME:$TODAY
+    docker tag $USERNAME/$NAME:$TODAY  $USERNAME/$NAME:latest
+    docker push $USERNAME/$NAME:$TODAY
+    docker push $USERNAME/$NAME:latest
+fi
